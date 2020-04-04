3909834a06fc6f757eebb7aa88c53e506905270c
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-04-04 14:22:09 -0500

    figure out how to do queries

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,24 @@
 import React from "react"
-import Layout from "../components/layout"
+import { graphql } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <Header>About Pandas Eating Lots</Header>
+    <Header>About {data.site.siteMetadata.title}</Header>
     <p>
       We're the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food.
     </p>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
