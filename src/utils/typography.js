71531acfcdd25553c77acf71551ef57dcfeb07a0
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-04-04 13:38:52 -0500

    finally fixed the fucking google fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,28 @@
 import Typography from "typography"
-import kirkhamTheme from "typography-theme-kirkham"
 
-const typography = new Typography(kirkhamTheme)
+const typography = new Typography({
+  baseFontSize: "20px",
+  baseLineHeight: 1.5,
+  headerFontFamily: [
+    "Archivo Black",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Lora", "serif"],
+  googleFonts: [
+    {
+      name: "Archivo Black",
+      styles: ["400"],
+    },
+    {
+      name: "Lora",
+      styles: ["400", "400i", "700", "700i"],
+    },
+  ],
+})
 
 export default typography
 export const rhythm = typography.rhythm
